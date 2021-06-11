b08c732e6681c63b2f462dfad55721ee966fa40c
edangolgithub <dangolevn@gmail.com>
2021-06-11 14:46:47 +0000

    yt

--- ecoweb/src/InventoryList.js
+++ ecoweb/src/InventoryList.js
@@ -151,6 +151,7 @@ export class InventoryList extends Component {
                 }
                 else {
                     alert(er)
+                    this.setState({ circling: false })
                 }
             })
 
