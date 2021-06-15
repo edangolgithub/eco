b69159653b696e43702dfb890049e703f0ca556b
edangolgithub <dangolevn@gmail.com>
2021-06-15 12:26:04 +0000

    kjhkj

---
+++ ecoweb/src/X.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class X extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default X
